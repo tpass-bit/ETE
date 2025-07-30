3c786efca7410b56e02acf1b4fad2a44b581078e
tpass-bit <narayanmawale83@gmail.com>
2025-07-30 15:18:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Firebase Configuration (separate file: firebase-config.js)
-// const firebaseConfig = {
-//     apiKey: "YOUR_API_KEY",
-//     authDomain: "YOUR_AUTH_DOMAIN",
-//     projectId: "YOUR_PROJECT_ID",
-//     storageBucket: "YOUR_STORAGE_BUCKET",
-//     messagingSenderId: "YOUR_SENDER_ID",
-//     appId: "YOUR_APP_ID"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBPF1VE82Y3VkZe6IibjqKxBC-XHjM_Wco",
+  authDomain: "chat-2024-ff149.firebaseapp.com",
+  databaseURL: "https://chat-2024-ff149-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-2024-ff149",
+  storageBucket: "chat-2024-ff149.appspot.com",
+  messagingSenderId: "146349109253",
+  appId: "1:146349109253:web:e593afbf0584762519ac6c" // Changed from android: to web:
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
